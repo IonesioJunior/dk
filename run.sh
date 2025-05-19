3f0104c5317dbab898b05b8537980a5b2af7c85b
Ionesio Junior <ionesiojr@gmail.com>
2025-05-19 14:43:09 -0300

    fix: set default SYFTBOX_ASSIGNED_PORT to 8081 in run.sh

--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ uv venv -p 3.12
 uv pip install -r requirements.txt
 
 # Set default port if not provided
-SYFTBOX_ASSIGNED_PORT=${SYFTBOX_ASSIGNED_PORT:-8081}
+# SYFTBOX_ASSIGNED_PORT=${SYFTBOX_ASSIGNED_PORT:-8081}
+SYFTBOX_ASSIGNED_PORT=8081
 uv run uvicorn app:syftbox.app --host 0.0.0.0 --port $SYFTBOX_ASSIGNED_PORT --reload
